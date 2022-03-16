d2694c08638a20826d705a64cf4769b6e03dcc87
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-03-16 17:59:46 -0400

    feat(Root): update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //funcion que retorna los layouts con page o solo page
+  const getLayout = Component.getLayout || ((page) => page)
+
+  // return (
+  //   <Component {...pageProps} />
+  // )
+  return getLayout(<Component{...pageProps} />)
 }
 
 export default MyApp
